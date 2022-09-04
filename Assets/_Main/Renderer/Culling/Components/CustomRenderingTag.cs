41cfced31b847d40e916f4f75740d5e1239450e0
Bartu Baş <bartubas99@gmail.com>
2022-09-04 11:13:39 +0300

    Add CustomRenderingTag

---
+++ Assets/_Main/Renderer/Culling/Components/CustomRenderingTag.cs
@@ -0,0 +1,12 @@
+﻿using Unity.Entities;
+
+namespace DotsLibrary.Rendering
+{
+	/// <summary>
+	/// Objects with this tag will be Rendered with the Custom Renderer.
+	/// </summary>
+	[GenerateAuthoringComponent]
+	public class CustomRenderingTag : IComponentData
+	{
+	}
+}
