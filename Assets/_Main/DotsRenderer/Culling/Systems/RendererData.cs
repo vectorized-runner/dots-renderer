8ee71144474d859f1d7ae16bf470baa58dbd43aa
Bartu Baş <bartubas99@gmail.com>
2022-09-04 14:40:55 +0300

    Caching render meshes, and registering render mesh indices

---
+++ Assets/_Main/DotsRenderer/Culling/Systems/RendererData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace DotsRenderer
+{
+	public static class RendererData
+	{
+		public static List<RenderMesh> RenderMeshList;
+		static HashSet<RenderMesh> RenderMeshes;
+
+		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+		static void Initialize()
+		{
+			RenderMeshList = new List<RenderMesh>();
+			RenderMeshes = new HashSet<RenderMesh>();
+		}
+
+		public static RenderMeshIndex RegisterRenderMeshAndGetIndex(in RenderMesh renderMesh)
+		{
+			if(RenderMeshes.Add(renderMesh))
+			{
+				var index = RenderMeshList.Count;
+				RenderMeshList.Add(renderMesh);
+				return new RenderMeshIndex(index);
+			}
+
+			// TODO-Optimize IndexOf here if required. 
+			return new RenderMeshIndex(RenderMeshList.IndexOf(renderMesh));
+		}
+	}
+}
