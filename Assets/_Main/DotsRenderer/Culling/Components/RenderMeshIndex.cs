d1385680ceca85437847a626a1c9f566a921dcf1
Bartu Baş <bartubas99@gmail.com>
2022-09-04 13:30:17 +0300

    RenderMeshIndex IEquatable

---
+++ Assets/_Main/DotsRenderer/Culling/Components/RenderMeshIndex.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Unity.Entities;
+
+namespace DotsRenderer
+{
+	[GenerateAuthoringComponent]
+	public readonly struct RenderMeshIndex : IComponentData, IEquatable<RenderMeshIndex>
+	{
+		public readonly int Value;
+
+		public RenderMeshIndex(int value)
+		{
+			Value = value;
+		}
+
+		public bool Equals(RenderMeshIndex other)
+		{
+			return Value == other.Value;
+		}
+
+		public override bool Equals(object obj)
+		{
+			return obj is RenderMeshIndex other && Equals(other);
+		}
+
+		public override int GetHashCode()
+		{
+			return Value;
+		}
+
+		public static bool operator ==(RenderMeshIndex left, RenderMeshIndex right)
+		{
+			return left.Equals(right);
+		}
+
+		public static bool operator !=(RenderMeshIndex left, RenderMeshIndex right)
+		{
+			return !left.Equals(right);
+		}
+	}
+}
