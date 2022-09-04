084792eedb4e6b65bb72b967593c54e62b3987fd
Bartu Baş <bartubas99@gmail.com>
2022-09-04 14:26:36 +0300

    Initial Batch Renderer

--- Assets/_Main/DotsRenderer/Culling/Systems/RenderingSystem.cs
+++ Assets/_Main/DotsRenderer/Culling/Systems/RenderingSystem.cs
@@ -1,78 +1,61 @@
-﻿using Unity.Burst;
-using Unity.Collections;
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using Unity.Entities;
 using Unity.Transforms;
 using UnityEngine;
 
 namespace DotsRenderer
 {
-	// TODO: Move Jobs to here, easier to see flow of logic, name the Jobs properly
-	// TODO: Use Static Gizmo Drawer
-	// TODO: SIMD Plane-AABB Check
-	// TODO: Continue from here.
-	// In order for this to work, we need the remaining things:
-	// RenderBounds should be static, WorldRenderBounds for entities needs to be calculated
-	// using for each WorldRenderBounds in the Chunk, ChunkWorldRenderBounds needs to be calculated
-	// When a Chunk is added/changed, if it has RenderMesh shared component, we need to add ChunkWorldRenderBounds to it
-	// When a Chunk is added/changed, if it has RenderMesh shared component, we need to add/remove ChunkRenderMeshIndex component to it
-	// We need to know the amount of Unique Render Meshes, so we can calculate the RenderMesh -> ChunkRenderMeshIndex mapping
-	// This job is still very inefficient:
-	// for each entity we want to render we're doing a HashMap addition, it should be list/array addition
-	// We should remove the HasComponent check for ChunkWorldRenderBounds, the scheduler needs to provide the correct query
-	// How can we sort the RenderBatches? (NativeArray<Chunk>, NativeList<int>, input is the Chunks with same RenderMesh, output is Render index list)
-	[BurstCompile]
-	public struct CollectVisibleRenderMatricesJob : IJobChunk
-	{
-		[ReadOnly]
-		public NativeArray<Plane> CameraFrustumPlanes;
-
-		[ReadOnly]
-		public ComponentTypeHandle<ChunkWorldRenderBounds> ChunkWorldRenderBoundsHandle;
-
-		[ReadOnly]
-		public ComponentTypeHandle<WorldRenderBounds> WorldRenderBoundsHandle;
-
-		[ReadOnly]
-		public ComponentTypeHandle<LocalToWorld> LocalToWorldHandle;
-
-		[ReadOnly]
-		public ComponentTypeHandle<ChunkRenderMeshIndex> ChunkRenderMeshIndexHandle;
-
-		[WriteOnly]
-		public NativeParallelMultiHashMap<ChunkRenderMeshIndex, Matrix4x4> RenderMatricesByRenderMeshIndex;
-
-		public void Execute(ArchetypeChunk chunk, int chunkIndex, int firstEntityIndex)
-		{
-			if(chunk.HasChunkComponent(ChunkWorldRenderBoundsHandle))
-			{
-				var worldRenderBounds = chunk.GetChunkComponentData(ChunkWorldRenderBoundsHandle);
-				
-				if(RMath.IsVisibleByCameraFrustum(CameraFrustumPlanes, worldRenderBounds.AABB))
-				{
-					var worldRenderBoundsArray = chunk.GetNativeArray(WorldRenderBoundsHandle);
-					var localToWorldArray = chunk.GetNativeArray(LocalToWorldHandle);
-					var renderMeshIndex = chunk.GetChunkComponentData(ChunkRenderMeshIndexHandle);
-
-					for(int i = 0; i < chunk.Count; i++)
-					{
-						if(RMath.IsVisibleByCameraFrustum(CameraFrustumPlanes, worldRenderBoundsArray[i].AABB))
-						{
-							RenderMatricesByRenderMeshIndex.Add(renderMeshIndex, localToWorldArray[i].Value);
-						}
-					}
-				}
-			}
-		}
-	}
-
 	[UpdateInGroup(typeof(PresentationSystemGroup))]
+	[UpdateAfter(typeof(CullingSystem))]
 	public partial class RenderingSystem : SystemBase
 	{
+		public List<RenderMesh> UniqueRenderMeshes;
+
+		static Matrix4x4[] MatrixCache;
+		CullingSystem CullingSystem;
+
+		protected override void OnCreate()
+		{
+			CullingSystem = World.GetExistingSystem<CullingSystem>();
+		}
+
 		protected override void OnUpdate()
 		{
-			// How do I sort chunks by their shared component?
+			var matricesByRenderMeshIndex = CullingSystem.MatricesByRenderMeshIndex;
+			var renderMeshCount = matricesByRenderMeshIndex.Length;
 
-			// Graphics.DrawMeshInstanced();
+			for(int renderMeshIndex = 0; renderMeshIndex < renderMeshCount; renderMeshIndex++)
+			{
+				const int maxDrawCountPerBatch = 1023;
+				var matrices = matricesByRenderMeshIndex[renderMeshIndex];
+				var renderMesh = UniqueRenderMeshes[renderMeshIndex];
+				var drawCount = matrices.Length;
+				var fullBatchCount = drawCount / maxDrawCountPerBatch;
+				var lastBatchDrawCount = drawCount % maxDrawCountPerBatch;
+				ReadOnlySpan<LocalToWorld> localToWorldSlice;
+				int batchIndex;
+
+				for(batchIndex = 0; batchIndex < fullBatchCount; batchIndex++)
+				{
+					localToWorldSlice = matrices.AsReadOnlySpan(batchIndex * maxDrawCountPerBatch, maxDrawCountPerBatch);
+					DrawMeshInstanced(renderMesh, localToWorldSlice);
+				}
+
+				localToWorldSlice = matrices.AsReadOnlySpan(batchIndex * maxDrawCountPerBatch, lastBatchDrawCount);
+				DrawMeshInstanced(renderMesh, localToWorldSlice);
+			}
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		static void DrawMeshInstanced(in RenderMesh renderMesh, ReadOnlySpan<LocalToWorld> matrices)
+		{
+			var typeCast = MemoryMarshal.Cast<LocalToWorld, Matrix4x4>(matrices);
+			typeCast.CopyTo(MatrixCache);
+			Graphics.DrawMeshInstanced(renderMesh.Mesh, renderMesh.SubMeshIndex, renderMesh.Material, MatrixCache,
+				matrices.Length);
 		}
 	}
 }
