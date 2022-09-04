d350d4bf8bdc2706ab68b380d15606a5eafb6671
Bartu Baş <bartubas99@gmail.com>
2022-09-04 15:32:10 +0300

    Add: No stack trace logging

---
+++ Assets/_Main/DotsRenderer/Culling/Utilities/FastLog.cs
@@ -0,0 +1,14 @@
+﻿using Unity.Burst;
+using UnityEngine;
+
+namespace DotsRenderer
+{
+	public class FastLog
+	{
+		[BurstDiscard]
+		public static void Info(string message, Object context = null)
+		{
+			Debug.LogFormat(LogType.Log, LogOption.NoStacktrace, context, message);
+		}
+	}
+}
