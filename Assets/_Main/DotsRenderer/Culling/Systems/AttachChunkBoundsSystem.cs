3df6d2f9399deafa1ed4e7e0b4bfcfbe7ab0505b
Bartu Baş <bartubas99@gmail.com>
2022-09-06 22:53:52 +0300

    Add: ChunkBounds component for Entities coming from a Subscene

---
+++ Assets/_Main/DotsRenderer/Culling/Systems/AttachChunkBoundsSystem.cs
@@ -0,0 +1,25 @@
+﻿using Unity.Entities;
+
+namespace DotsRenderer
+{
+	public partial class AttachChunkBoundsSystem : SystemBase
+	{
+		EntityQuery RequireChunkBoundsQuery;
+
+		protected override void OnCreate()
+		{
+			RequireChunkBoundsQuery =
+				GetEntityQuery(
+					ComponentType.ReadOnly<SceneSection>(),
+					ComponentType.ChunkComponentExclude<ChunkWorldRenderBounds>());
+		}
+
+		protected override void OnUpdate()
+		{
+			if(RequireChunkBoundsQuery.CalculateEntityCount() == 0)
+				return;
+
+			EntityManager.AddChunkComponentData(RequireChunkBoundsQuery, new ChunkWorldRenderBounds());
+		}
+	}
+}
