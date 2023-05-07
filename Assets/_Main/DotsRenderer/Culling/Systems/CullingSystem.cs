dcb7465ff9fc90eb913aecd160fc1e8061ef8142
Bartu Baş <bartubas99@gmail.com>
2023-05-07 21:58:44 +0300

    WIP: Culling system

---
+++ Assets/_Main/DotsRenderer/Culling/Systems/CullingSystem.cs
@@ -0,0 +1,48 @@
+﻿using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Entities;
+using Unity.Transforms;
+
+namespace DotsRenderer
+{
+	[UpdateInGroup(typeof(PresentationSystemGroup))]
+	[UpdateAfter(typeof(CalculateWorldRenderBoundsSystem))]
+	public partial class CullingSystem : SystemBase
+	{
+		public NativeList<UnsafeList<LocalToWorld>> MatricesByRenderMeshIndex;
+
+		protected override void OnCreate()
+		{
+			// TODO: We need to Initialize and Grow this when required!
+			MatricesByRenderMeshIndex = new NativeList<UnsafeList<LocalToWorld>>(Allocator.Persistent);
+		}
+
+		protected override void OnDestroy()
+		{
+			for(int i = 0; i < MatricesByRenderMeshIndex.Length; i++)
+			{
+				MatricesByRenderMeshIndex[i].Dispose();
+			}
+
+			MatricesByRenderMeshIndex.Dispose();
+		}
+
+		protected override void OnUpdate()
+		{
+			var frustumPlanes = World.GetExistingSystem<CalculateCameraFrustumPlanesSystem>().NativeFrustumPlanes;
+
+			Entities.ForEach((in WorldRenderBounds worldRenderBounds,
+			                  in RenderMeshIndex renderMeshIndex,
+			                  in LocalToWorld localToWorld) =>
+			        {
+				        if(RMath.IsVisibleByCameraFrustum(frustumPlanes, worldRenderBounds.AABB))
+				        {
+					        ref var matrices = ref MatricesByRenderMeshIndex.ElementAsRef(renderMeshIndex.Value);
+					        // TODO-Optimization: This addition is not thread-safe, we need to figure out another way if we want to process in parallel.
+					        matrices.Add(localToWorld);
+				        }
+			        })
+			        .Schedule();
+		}
+	}
+}
