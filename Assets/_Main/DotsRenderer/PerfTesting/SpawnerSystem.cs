9eb0588825eda33b89cd7c8fb3d3ddc4fb64a634
Bartu Baş <bartubas99@gmail.com>
2023-05-07 21:58:44 +0300

    Fix: Set L2W instead of Translation

--- Assets/_Main/DotsRenderer/PerfTesting/SpawnerSystem.cs
+++ Assets/_Main/DotsRenderer/PerfTesting/SpawnerSystem.cs
@@ -18,15 +18,17 @@ namespace DotsRenderer.PerfTesting
 			var spawnCount = spawnData.CountX * spawnData.CountZ;
 			var spawnedEntities = EntityManager.Instantiate(spawnData.Entity, spawnCount, Allocator.TempJob);
 			var countX = spawnData.CountX;
-			
-			for(int i = 0; i < spawnedEntities.Length; i++)
+
+			for(int index = 0; index < spawnedEntities.Length; index++)
 			{
-				var iX = spawnCount / countX;
-				var iZ = spawnCount % countX;
-				var translation = new Translation { Value = new float3(iX, 0f, iZ) };
-				EntityManager.SetComponentData(spawnedEntities[i], translation);
+				var iX = index / countX;
+				var iZ = index % countX;
+				var l2w = EntityManager.GetComponentData<LocalToWorld>(spawnedEntities[index]);
+				var position = new float3(iX, 0f, iZ) * spawnData.Spacing;
+				l2w.Value.c3.xyz = position;
+				EntityManager.SetComponentData(spawnedEntities[index], l2w);
 			}
-			
+
 			EntityManager.DestroyEntity(GetSingletonEntity<SpawnData>());
 		}
 	}
