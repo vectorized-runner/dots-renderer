cdf2d35aae4826d031581f6c97659e20fe3114b3
Bartu Baş <bartubas99@gmail.com>
2023-05-07 21:58:44 +0300

    Add: Proper UI spawner

---
+++ Assets/_Main/DotsRenderer/Demo/SpawnRegisterer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+namespace DotsRenderer.Demo
+{
+	[GenerateAuthoringComponent]
+	public struct EntityToSpawn : IComponentData
+	{
+		public Entity Value;
+	}
+
+	public class SpawnRegisterer : MonoBehaviour, IDeclareReferencedPrefabs, IConvertGameObjectToEntity
+	{
+		public GameObject Prefab;
+
+		public void DeclareReferencedPrefabs(List<GameObject> referencedPrefabs)
+		{
+			referencedPrefabs.Add(Prefab);
+		}
+
+		public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+		{
+			var primaryEntity = conversionSystem.GetPrimaryEntity(Prefab);
+			dstManager.AddComponentData(entity, new EntityToSpawn
+			{
+				Value = primaryEntity
+			});
+		}
+	}
+}
---
+++ Assets/_Main/DotsRenderer/Demo/UISpawner.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using DotsRenderer.PerfTesting;
+using TMPro;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DotsRenderer.Demo
+{
+	public class UISpawner : MonoBehaviour
+	{
+		public TMP_InputField InputField;
+		public Button SpawnButton;
+		public float Spacing;
+
+		void Start()
+		{
+			SpawnButton.onClick.AddListener(Spawn);
+		}
+
+		void OnDestroy()
+		{
+			SpawnButton.onClick.RemoveListener(Spawn);
+		}
+
+		void Spawn()
+		{
+			var text = InputField.text;
+			if(!int.TryParse(text, out var spawnCount))
+			{
+				Debug.LogError($"Can't parse int from text: '{text}'");
+				return;
+			}
+
+			var countSqrt = (int)math.sqrt(spawnCount);
+			var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+			var query = entityManager.CreateEntityQuery(typeof(EntityToSpawn));
+			var singleton = query.GetSingletonEntity();
+			var entityToSpawn = entityManager.GetComponentData<EntityToSpawn>(singleton).Value;
+
+			var entity = entityManager.CreateEntity();
+			entityManager.AddComponentData(entity, new SpawnData
+			{
+				CountX = countSqrt,
+				CountZ = countSqrt,
+				Entity = entityToSpawn,
+				Spacing = Spacing,
+			});
+
+			Debug.Log("Spawn Entities!");
+		}
+	}
+}
