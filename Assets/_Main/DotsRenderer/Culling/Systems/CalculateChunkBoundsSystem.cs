cde41c88f40049bef6dd819811bbc705ad9358e9
Bartu Baş <bartubas99@gmail.com>
2023-05-07 21:58:45 +0300

    WIP: Calculate chunk bounds system

--- Assets/_Main/DotsRenderer/Culling/Systems/CalculateChunkBoundsSystem.cs
+++ Assets/_Main/DotsRenderer/Culling/Systems/CalculateChunkBoundsSystem.cs
@@ -1,1 +1,62 @@
-﻿
+﻿using System;
+using System.Runtime.CompilerServices;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace DotsRenderer
+{
+	[BurstCompile]
+	public struct CalculateChunkBoundsJob : IJobChunk
+	{
+		public ComponentTypeHandle<ChunkWorldRenderBounds> ChunkWorldRenderBoundsHandle;
+
+		[ReadOnly]
+		public ComponentTypeHandle<WorldRenderBounds> WorldRenderBoundsHandle;
+
+		public void Execute(ArchetypeChunk chunk, int chunkIndex, int firstEntityIndex)
+		{
+			var worldRenderBoundsArray = chunk.GetNativeArray(WorldRenderBoundsHandle);
+			var resultAABB = new AABB();
+
+			// TODO-Optimize: This can be optimized by unrolling and removing 'resultAABB' data dependency?
+			// Instead check indices 0-1, 2-3, 4-5 etc...
+			for(int i = 0; i < worldRenderBoundsArray.Length; i++)
+			{
+				resultAABB = Encapsulate(resultAABB, worldRenderBoundsArray[i].AABB);
+			}
+
+			chunk.SetChunkComponentData(ChunkWorldRenderBoundsHandle, new ChunkWorldRenderBounds { AABB = resultAABB });
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		AABB Encapsulate(AABB first, AABB second)
+		{
+			// TODO-Implement this.
+			throw new NotImplementedException();
+		}
+	}
+
+	[UpdateInGroup(typeof(PresentationSystemGroup))]
+	[UpdateAfter(typeof(AttachChunkBoundsSystem))]
+	public partial class CalculateChunkBoundsSystem : SystemBase
+	{
+		EntityQuery ChangedChunksQuery;
+
+		protected override void OnCreate()
+		{
+			ChangedChunksQuery = GetEntityQuery(ComponentType.ReadWrite<ChunkWorldRenderBounds>());
+			// We only need to recalculate if any of the 'WorldRenderBounds' of Entities is changed
+			ChangedChunksQuery.SetChangedVersionFilter(ComponentType.ReadOnly<WorldRenderBounds>());
+		}
+
+		protected override void OnUpdate()
+		{
+			Dependency = new CalculateChunkBoundsJob
+			{
+				ChunkWorldRenderBoundsHandle = GetComponentTypeHandle<ChunkWorldRenderBounds>(),
+				WorldRenderBoundsHandle = GetComponentTypeHandle<WorldRenderBounds>(),
+			}.Schedule(ChangedChunksQuery, Dependency);
+		}
+	}
+}
