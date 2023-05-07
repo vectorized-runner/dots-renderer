515e37b6b59d9172ff068e567c0829f60c05668f
Bartu Baş <bartubas99@gmail.com>
2023-05-07 21:58:45 +0300

    FastLog Warning and Error types

--- Assets/_Main/DotsRenderer/Culling/Utilities/FastLog.cs
+++ Assets/_Main/DotsRenderer/Culling/Utilities/FastLog.cs
@@ -3,12 +3,24 @@ using UnityEngine;
 
 namespace DotsRenderer
 {
-	public class FastLog
+	public static class FastLog
 	{
 		[BurstDiscard]
 		public static void Info(string message, Object context = null)
 		{
 			Debug.LogFormat(LogType.Log, LogOption.NoStacktrace, context, message);
+		}
+
+		[BurstDiscard]
+		public static void Warning(string message, Object context = null)
+		{
+			Debug.LogFormat(LogType.Warning, LogOption.NoStacktrace, context, message);
+		}
+
+		[BurstDiscard]
+		public static void Error(string message, Object context = null)
+		{
+			Debug.LogFormat(LogType.Error, LogOption.NoStacktrace, context, message);
 		}
 	}
 }
