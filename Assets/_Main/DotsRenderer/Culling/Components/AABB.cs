9ad42796dc8f04a96f84b8c69d6e8ddd914171bb
Bartu Baş <bartubas99@gmail.com>
2023-05-07 21:58:45 +0300

    AABB.Encapsulate

--- Assets/_Main/DotsRenderer/Culling/Components/AABB.cs
+++ Assets/_Main/DotsRenderer/Culling/Components/AABB.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using Unity.Mathematics;
 
 namespace DotsRenderer
@@ -8,5 +9,18 @@ namespace DotsRenderer
 	{
 		public float3 Center;
 		public float3 Extents;
+		
+		public float3 Min => Center - Extents;
+		public float3 Max => Center + Extents;
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static AABB FromMinMax(float3 min, float3 max)
+		{
+			return new AABB
+			{
+				Center = (min + max) * 0.5f,
+				Extents = (max - min) * 0.5f,
+			};
+		}
 	}
 }
